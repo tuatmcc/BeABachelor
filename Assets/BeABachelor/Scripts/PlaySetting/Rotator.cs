35dcc5025177d6b49106f8bdebc7b4d0325256b8
uPiscium <upiscium@gmail.com>
2024-03-18 12:08:18 +0900

    Add selector base and materials, and controll scripts.

--- Assets/BeABachelor/Scripts/PlaySetting/Rotator.cs
+++ Assets/BeABachelor/Scripts/PlaySetting/Rotator.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSelector : MonoBehaviour
+public class Rotator : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0, 0.5f, 0));
     }
 }
