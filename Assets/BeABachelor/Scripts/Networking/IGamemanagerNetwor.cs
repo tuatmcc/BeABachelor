5c90d6b2e903b774c27f2276809530852632958f
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-03-19 15:43:13 +0900

    IGamemanagerNetwor

---
+++ Assets/BeABachelor/Scripts/Networking/IGamemanagerNetwor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace BeABachelor.Networking
+{
+    public interface IGamemanagerNetwor
+    {
+        int GetTickCount();
+        void SetTickCount(int tickCount);
+        int GetScore();
+        void SetScore(int score);
+        bool[] GetEnableItems();
+        void SetEnableItems(int index, bool value);
+        Vector3 GetPlayerPosition();
+        void SetPlayerPosition(Vector3 position);
+        Vector3 GetEnemyPosition();
+        void SetEnemyPosition(Vector3 position);
+    }
+}
