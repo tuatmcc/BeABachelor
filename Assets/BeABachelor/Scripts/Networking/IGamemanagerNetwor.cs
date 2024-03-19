d9930b7e0eb30dde6585ade04d831ab9d6e6b6d9
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-03-19 22:43:06 +0900

    IGamemanagerNetwor.cs

--- Assets/BeABachelor/Scripts/Networking/IGamemanagerNetwor.cs
+++ Assets/BeABachelor/Scripts/Networking/IGamemanagerNetwor.cs
@@ -8,11 +8,13 @@ namespace BeABachelor.Networking
         void SetTickCount(int tickCount);
         int GetScore();
         void SetScore(int score);
+        void AddScore(int score);
         bool[] GetEnableItems();
         void SetEnableItems(int index, bool value);
         Vector3 GetPlayerPosition();
         void SetPlayerPosition(Vector3 position);
         Vector3 GetEnemyPosition();
         void SetEnemyPosition(Vector3 position);
+        bool IsHakken();
     }
 }
