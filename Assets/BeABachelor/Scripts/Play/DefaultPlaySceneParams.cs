e756fa84bdf4cbca7669b1f148a102dc70223f1e
kitashimauni <110659558+kitashimauni@users.noreply.github.com>
2024-03-20 17:23:38 +0900

    スタミナの内部実装完了

--- Assets/BeABachelor/Scripts/Play/DefaultPlaySceneParams.cs
+++ Assets/BeABachelor/Scripts/Play/DefaultPlaySceneParams.cs
@@ -11,6 +11,8 @@ namespace BeABachelor.Play
         public static float RunningSpeed = 1.5f;
         // スタミナを使い切った時の移動速度倍率
         public static float NoStaminaSpeed = 0.7f;
+        // スタミナの量(10で1秒)
+        public static long StaminaMax = 70;
         // 向きを変えたときの回転速度
         public static float RotateSpeed = 10.0f;
         // 始めのカウントダウンの長さ
