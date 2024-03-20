b3f7a18bb3f76a56224b165e5c50c93c493cc2da
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-03-20 23:59:32 +0900

    TickData

---
+++ Assets/BeABachelor/Scripts/Networking/TickData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace BeABachelor.Networking
+{
+    public record TickData : InteractionObject
+    {
+        public bool[] EnableItems;
+        public Vector3 EnemyPosition;
+        public Vector3 PlayerPosition;
+        
+        public TickData(int tickCount, bool[] enableItems, Vector3 playerPosition, Vector3 enemyPosition) => (ReRequestFlag, TickCount, EnableItems, PlayerPosition, EnemyPosition) = (false, tickCount, enableItems, playerPosition, enemyPosition);
+        public override string ToString() => $"TickData: {TickCount}, {EnableItems}, {PlayerPosition}, {EnemyPosition}";
+    }
+}
