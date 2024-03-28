128da250c08270148f200297d3c1c12b068254ed
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-03-28 14:43:55 +0900

    test

--- Assets/BeABachelor/Scripts/Networking/Play/Test/NetworkManagerConnectionTest.cs
+++ Assets/BeABachelor/Scripts/Networking/Play/Test/NetworkManagerConnectionTest.cs
@@ -9,11 +9,11 @@ namespace BeABachelor.Networking.Play.Test
     public class NetworkManagerConnectionTest : MonoBehaviour
     {
         [SerializeField] private NetworkManager networkManager1;
-        
+
         private void Start()
         {
-            networkManager1.ConnectAsync().Forget();
-            UniTask.WaitUntil(() => networkManager1.IsConnected).Forget();
+        //     networkManager1.ConnectAsync().Forget();
+        //     UniTask.WaitUntil(() => networkManager1.IsConnected).Forget();
             
             // Hoge().Forget();
             // Fuga().Forget();
