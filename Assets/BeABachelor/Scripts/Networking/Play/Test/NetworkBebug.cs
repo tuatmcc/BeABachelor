398485e159de0c2a1faaa38f8312f3978b7d258e
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-03-30 16:27:43 +0900

    debug

---
+++ Assets/BeABachelor/Scripts/Networking/Play/Test/NetworkBebug.cs
@@ -0,0 +1,36 @@
+﻿using BeABachelor.Interface;
+using BeABachelor.Networking.Interface;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Zenject;
+
+namespace BeABachelor.Networking.Play.Test
+{
+    public class NetworkBebug : MonoBehaviour
+    {
+        [SerializeField] private string ipAddress;
+        [SerializeField] private int clientPort;
+        [SerializeField] private int remotePort;
+        [SerializeField] private PlayerType playerType;
+        [SerializeField] private NetworkState networkState = NetworkState.Disconnected;
+        
+        [Inject] private INetworkManager _networkManager;
+        [Inject] private IGameManager _gameManager;
+        
+        [ContextMenu("Change Scene")]
+        public void ChangeScene()
+        {
+            _networkManager.OnConnected += _ =>
+            {
+                networkState = NetworkState.Connected;
+                _gameManager.GameState = GameState.Ready;
+            };
+            _networkManager.OnConnecting += _ =>
+            {
+                networkState = NetworkState.Connecting;
+            };
+            _gameManager.PlayerType = playerType;
+            _networkManager.ConnectAsync(false, ipAddress, remotePort, clientPort).Forget();
+        }
+    }
+}
