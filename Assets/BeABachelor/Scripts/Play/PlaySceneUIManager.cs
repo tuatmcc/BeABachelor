4a6553940f60d5593ff50539c592dee1830c9a70
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-04-03 00:08:22 +0900

    UI

---
+++ Assets/BeABachelor/Scripts/Play/PlaySceneUIManager.cs
@@ -0,0 +1,26 @@
+﻿using BeABachelor.Interface;
+using BeABachelor.Networking.Interface;
+using UnityEngine;
+using Zenject;
+
+namespace BeABachelor.Play
+{
+    public class PlaySceneUIManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject waitOpponentPanel;
+        
+        [Inject] private INetworkManager _networkManager;
+        [Inject] private IGameManager _gameManager;
+        
+        private void OnOpponentReady()
+        {
+            waitOpponentPanel.SetActive(false);
+        }
+        private void Start()
+        {
+            if (_gameManager.PlayType != PlayType.Multi) return;
+            waitOpponentPanel.SetActive(true);
+            _networkManager.OpponentReadyEvent += OnOpponentReady;
+        }
+    }
+}
