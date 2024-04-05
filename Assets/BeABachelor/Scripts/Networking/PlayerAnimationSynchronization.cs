ec0c7146bf34f54d054f25eaaef999959ae6cd57
sugawa2018616 <96975428+sugawa197203@users.noreply.github.com>
2024-04-05 10:41:49 +0900

    animation network

---
+++ Assets/BeABachelor/Scripts/Networking/PlayerAnimationSynchronization.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace BeABachelor.Networking
+{
+    public class PlayerAnimationSynchronization : MonoSynchronization
+    {
+        private Animator _animator;
+        private Rigidbody _rigidbody;
+        
+        private int _animIDSpeed = Animator.StringToHash("Speed");
+        private int _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
+        
+        public override byte[] ToBytes()
+        {
+            using var writer = new BinaryWriter(new MemoryStream(8));
+            writer.Write(_animator.GetFloat(_animIDSpeed));
+            writer.Write(_animator.GetFloat(_animIDMotionSpeed));
+            return ((MemoryStream)writer.BaseStream).ToArray();
+        }
+
+        public override void FromBytes(byte[] bytes)
+        {
+            if(!UseReceivedData) return;
+            using var reader = new BinaryReader(new MemoryStream(bytes));
+            _animator.SetFloat(_animIDSpeed, reader.ReadSingle());
+            _animator.SetFloat(_animIDMotionSpeed, reader.ReadSingle());
+        }
+    }
+}
--- Assets/BeABachelor/Scripts/Play/Player/Controller/PlayerAnimationController.cs
+++ Assets/BeABachelor/Scripts/Play/Player/Controller/PlayerAnimationController.cs
@@ -1,3 +1,4 @@
+using BeABachelor.Networking;
 using UnityEngine;
 
 namespace BeABachelor.Play.Player
@@ -8,7 +9,7 @@ namespace BeABachelor.Play.Player
     {
         private Animator _animator;
         private Rigidbody _rigidbody;
-
+        private PlayerAnimationSynchronization _playerAnimationSynchronization;
 
         private int _animIDSpeed = Animator.StringToHash("Speed");
         private int _animIDGrounded = Animator.StringToHash("Grounded");
@@ -18,11 +19,13 @@ namespace BeABachelor.Play.Player
         {
             _animator = GetComponent<Animator>();
             _rigidbody = GetComponent<Rigidbody>();
+            _playerAnimationSynchronization = GetComponent<PlayerAnimationSynchronization>();
             _animator.SetBool(_animIDGrounded, true);
         }
 
         private void Update()
         {
+            if(_playerAnimationSynchronization.UseReceivedData) return;
             _animator.SetFloat(_animIDSpeed, _rigidbody.velocity.magnitude);
             _animator.SetFloat(_animIDMotionSpeed, _rigidbody.velocity.magnitude * 0.1f);
         }
