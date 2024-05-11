dbc3988d1f07e18038ae9494e2c3d7e5ccf87069
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-05-11 17:47:59 +0900

    disconnect

--- Assets/BeABachelor/Scripts/Play/PlaySceneManager.cs
+++ Assets/BeABachelor/Scripts/Play/PlaySceneManager.cs
@@ -197,6 +197,7 @@ namespace BeABachelor.Play
         public async UniTask FinishPlay()
         {
             _cts?.Cancel();
+            _networkManager.Disconnect();
             if(_gameManager.GameState == GameState.Playing && !_sceneChangeFlag)
             {
                 _sceneChangeFlag = true;
