f4a38b0567182490b3b6e4d3b83f9a811ec97648
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-05-12 16:33:48 +0900

    network

---
+++ Assets/BeABachelor/Scripts/Networking/ScrollItemSynchronization.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using BeABachelor.Play.Items;
+using UnityEngine;
+
+namespace BeABachelor.Networking
+{
+    public class ScrollItemSynchronization : MonoSynchronization
+    {
+        private List<int> _sendDeletedItemIDs;
+        [SerializeField] private ItemManager _itemManager;
+        
+        private void Start()
+        {
+            _sendDeletedItemIDs = new List<int>();
+            _itemManager.OnItemHit += id => _sendDeletedItemIDs.Add(id);
+            base.Start();
+        }
+        
+        public override byte[] ToBytes()
+        {
+            if (_sendDeletedItemIDs.Count == 0) return BitConverter.GetBytes(0);
+            var ids = _sendDeletedItemIDs.ToArray();
+            _sendDeletedItemIDs.Clear();
+            var writer = new BinaryWriter(new MemoryStream(4 + ids.Length * 4));
+            writer.Write(ids.Length);
+            foreach (var id in ids)
+            {
+                writer.Write(id);
+            }
+            return ((MemoryStream)writer.BaseStream).ToArray();
+        }
+
+        public override void FromBytes(byte[] bytes)
+        {
+            var reader = new BinaryReader(new MemoryStream(bytes));
+            var count = reader.ReadInt32();
+            for (var i = 0; i < count; i++)
+            {
+                var id = reader.ReadInt32();
+                if(_itemManager.TryGetItemFromID(id, out var item))
+                {
+                    _itemManager.ItemHitNotify(id);
+                    Debug.Log($"ID : {id}");
+                    var scrollItem = item.GetComponent<ScoreItem>();
+                    scrollItem.DeleteEffect();
+                    scrollItem.GetScore2Enemy();
+                }
+            }
+        }
+    }
+}
