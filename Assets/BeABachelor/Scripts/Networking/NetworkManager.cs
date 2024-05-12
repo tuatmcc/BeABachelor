9791acbc27a38947441457b92186a40e64d531cd
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-05-13 00:22:45 +0900

    debug

--- Assets/BeABachelor/Scripts/Networking/NetworkManager.cs
+++ Assets/BeABachelor/Scripts/Networking/NetworkManager.cs
@@ -271,17 +271,8 @@ namespace BeABachelor.Networking
                 }
 
                 var reader = new BinaryReader(new MemoryStream(task.Result.Buffer));
-                if (reader.ReadByte() != 0xaa)
-                {
-                    Debug.Log("Invalid data");
-                    continue;
-                }
-
-                if (SynchronizationController == null)
-                {
-                    Debug.Log("SynchronizationController is null");
-                    continue;
-                }
+                if (reader.ReadByte() != 0xaa) continue;
+                if (SynchronizationController == null) continue;
                 // これ以降 PlayScene での処理
                 OpponentReady = true;
 
