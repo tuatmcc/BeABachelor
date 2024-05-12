d8b2b1a1052c2d770a6a7c64e4f3dcc467e2c3a9
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2024-05-13 00:23:12 +0900

    debug

--- Assets/BeABachelor/Scripts/Networking/NetworkManager.cs
+++ Assets/BeABachelor/Scripts/Networking/NetworkManager.cs
@@ -42,6 +42,7 @@ namespace BeABachelor.Networking
             get => _opponentReady;
             private set
             {
+                Debug.Log($"OpponentReady: {_opponentReady} -> {value}");
                 if (!_opponentReady && value)
                     OpponentReadyEvent?.Invoke();
             }
