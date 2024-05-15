208649bcb4b77424832130284f39bab636018738
kitashimauni <110659558+kitashimauni@users.noreply.github.com>
2024-05-16 01:46:14 +0900

    test

--- Assets/BeABachelor/Scripts/Networking/TransformSynchronization.cs
+++ Assets/BeABachelor/Scripts/Networking/TransformSynchronization.cs
@@ -1,11 +1,16 @@
-﻿using System.IO;
+using BeABachelor.Interface;
+using System.IO;
 using System.Net;
 using UnityEngine;
+using Zenject;
 
 namespace BeABachelor.Networking
 {
     public class TransformSynchronization : MonoSynchronization
     {
+        private Vector3 p = new Vector3();
+        private Quaternion r = Quaternion.identity;
+
         private void OnConnect(EndPoint _)
         {
                 if (TryGetComponent(out Rigidbody rb))
@@ -23,9 +28,17 @@ namespace BeABachelor.Networking
         private new void Start()
         {
             _networkManager.OnConnected += OnConnect;
+            p = transform.position;
+            r = transform.rotation;
             base.Start();
         }
-        
+
+        private void Update()
+        {
+            if (!UseReceivedData) return;
+            transform.position = p;
+            transform.rotation = r;
+        }
         private void OnDestroy()
         {
             _networkManager.OnConnected -= OnConnect;
@@ -50,9 +63,6 @@ namespace BeABachelor.Networking
         public override void FromBytes(byte[] bytes)
         {
             if(!UseReceivedData) return;
-            var t = transform;
-            var p = t.position;
-            var r = t.rotation;
             using var reader = new BinaryReader(new MemoryStream(bytes));
             p.x = reader.ReadSingle();
             p.y = reader.ReadSingle();
@@ -61,8 +71,6 @@ namespace BeABachelor.Networking
             r.y = reader.ReadSingle();
             r.z = reader.ReadSingle();
             r.w = reader.ReadSingle();
-            t.position = p;
-            t.rotation = r;
         }
     }
 }
